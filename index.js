9ee0305f28930ab0246ae22dd1900531d20e8c23
talal <ta440237@gmail.com>
2023-06-04 01:02:15 +0300

    commit message

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+//const batteryBatches = [4, 5, 6, 7, 9];
+
+const totalBatteries = batteryBatches.reduce((accumulator, current) => accumulator + current, 0);
